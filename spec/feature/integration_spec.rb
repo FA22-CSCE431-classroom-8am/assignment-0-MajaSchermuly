81b6e86fd9f2d47cc06853eef1d0ed405bcedb3e
MajaSchermuly <56056802+MajaSchermuly@users.noreply.github.com>
2022-09-12 14:19:52 -0500

    failed integration and unit bc only title attribute

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -10,3 +10,33 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('harry potter')
   end
 end
+
+RSpec.describe 'Adding an Author', type: :feature do
+    scenario 'valid inputs' do
+      visit new_book_path
+      fill_in 'Author', with: 'J. K. Rowling'
+      click_on 'Add Author'
+      visit authors_path
+      expect(page).to have_content('J. K. Rowling')
+    end
+  end
+
+  RSpec.describe 'Adding a Price', type: :feature do
+    scenario 'valid inputs' do
+      visit new_book_path
+      fill_in 'Price', with: '12.99'
+      click_on 'Add Price'
+      visit price_path
+      expect(page).to have_content('12.99')
+    end
+  end
+
+  RSpec.describe 'Adding a Date', type: :feature do
+    scenario 'valid inputs' do
+      visit new_book_path
+      fill_in 'Date', with: '01/01/2001'
+      click_on 'Add Date'
+      visit date_path
+      expect(page).to have_content('01/01/2001')
+    end
+  end
